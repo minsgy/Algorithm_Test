a7ac9e982800ffdcdc6afe7b96464f59f6f5e2b6
minsgy <alstjrchl97@naver.com>
2021-02-10 01:35:51 +0900

    BOJ 1080 미완 , BOJ 1339 완

---
+++ Greedy/1080.py
@@ -0,0 +1,13 @@
+# 행렬
+
+i, j = map(int, input().split())
+
+list1 = [list(map(int, list(input()))) for _ in range(i)]
+list2 = [list(map(int, list(input()))) for _ in range(i)]
+
+print(list1)
+
+# for i in range(i):
+#     for j in range(j) := list(int, input())
+
+# print(list1)
---
+++ Greedy/1339.py
@@ -0,0 +1,29 @@
+# 단어 수학
+import sys
+
+n = int(sys.stdin.readline())
+liner = []
+
+words = {}  # 알파벳 저장 딕셔너리 'A': 10000 자리수값을 구한다.
+for _ in range(n):
+    liner.append(list(sys.stdin.readline().rstrip()))
+
+liner.sort(key=lambda x: len(x), reverse=True)
+
+for i in range(len(liner)):
+    for j in range(len(liner[i])):
+        if liner[i][j] not in words:
+            words[liner[i][j]] = pow(10, len(liner[i])-j-1)
+        else:
+            words[liner[i][j]] += pow(10, len(liner[i])-j-1)
+
+sort_liner = sorted(words.items(), key=lambda x: x[1], reverse=True)
+result = 0
+num = 9
+
+for i in range(len(sort_liner)):
+    result += num * sort_liner[i][1]
+    num -= 1
+
+
+print(result)
