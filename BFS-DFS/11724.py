afcaad5867cd03d5850f419ddc1d4d344aee5d61
minsgy <alstjrchl97@naver.com>
2021-02-20 13:02:23 +0900

    BOJ 11724

--- BFS-DFS/11724.py
+++ BFS-DFS/11724.py
@@ -1,36 +1,29 @@
-# 연결 요소의 개수 (간 선 구분해야함.)
 import sys
-sys.setrecursionlimit(10000)
+# 연결 요소 개수
+N, M = map(int, sys.stdin.readline().split())
 
-n, m = map(int, sys.stdin.readline().rstrip().split())
-matrix = [[0]*(n+1) for _ in range(n+1)]
-visited = [False for _ in range(n+1)]
-count = 0
-
-
-def dfs(v):
-    visited[v] = True
-    for i in range(1, n+1):
-        if visited[v] == False and matrix[v][i] == 1:
-            dfs(i)
+adj = [[]for i in range(N+1)]
+for i in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    adj[a].append(b)
+    adj[b].append(a)
+visit_list = [0]*(N+1)
 
 
-for _ in range(m):
-    v, w = map(int, sys.stdin.readline().rstrip().split())
-    matrix[v][w] = matrix[w][v] = 1
+def bfs(v):
+    queue = [v]
+    while queue:
+        v = queue.pop(0)
+        for i in adj[v]:
+            if visit_list[i] == 0:
+                queue.append(i)
+                visit_list[i] = 1
 
 
-for i in range(1, n+1):
-    if visited[i] == False:
-        dfs(i)
-        count += 1
+answer = 0
+for i in range(1, N+1):
+    if visit_list[i] == 0:
+        bfs(i)
+        answer += 1
 
-print(count)
-
-
-# 0 0 0 0 0 0
-# 0 0 0 0 0 0
-# 0 0 0 0 0 0
-# 0 0 0 0 0 0
-# 0 0 0 0 0 0
-# 0 0 0 0 0 0
+sys.stdout.write(str(answer))
