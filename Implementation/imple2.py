9f3f40a349b0b54b92cdc4e4391c0ad2959b5ec7
minsgy <alstjrchl97@naver.com>
2021-05-20 01:12:16 +0900

    왕실의 나이트

--- Implementation/imple2.py
+++ Implementation/imple2.py
@@ -1,1 +1,27 @@
 # 왕실의 나이트
+
+point = input()
+
+step_value = [
+  [-2, -1],
+  [-1, -2],
+  [1, -2],
+  [2, -1],
+  [2, 1],
+  [1, 2],
+  [-1, 2],
+  [-2, 1]
+]
+
+row = int(point[1])
+cols = int(ord(point[0])) - int(ord('a')) + 1
+
+result = 0
+for x, y in step_value:
+  next_x = x + row
+  next_y = y + cols
+  if next_x >= 1 and next_x <= 8 and next_y >= 1 and next_y <= 8:
+    result += 1
+
+
+print(result)
