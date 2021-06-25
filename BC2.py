95bf0d5aeb946c68bc914efda22a643cc4f43a6f
minsgy <alstjrchl97@likelion.org>
2021-06-25 22:02:48 +0900

    BC 2문제

--- BC2.py
+++ BC2.py
@@ -1,3 +1,6 @@
 def solution(param0):
     types = {'BOOL': 1, 'SHORT': 2, 'FLOAT': 4, 'INT': 8, 'LONG': 16}
+    DataArr = []
     
+    for data_type in param0:
+        Data.append(types[data_type]) 
