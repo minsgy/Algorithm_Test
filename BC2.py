548f6d6560d95ef0d72c9310b6b5b511feb5fa0c
minsgy <alstjrchl97@naver.com>
2021-06-27 22:28:59 +0900

    업데이트

--- BC2.py
+++ BC2.py
@@ -1,6 +1,15 @@
 def solution(param0):
     types = {'BOOL': 1, 'SHORT': 2, 'FLOAT': 4, 'INT': 8, 'LONG': 16}
     DataArr = []
-    
+    size = []
+    answer = ''
     for data_type in param0:
-        Data.append(types[data_type]) 
+        if data_type == 'LONG':
+            result += types[data_type]
+            
+
+
+solution(['INT','INT','BOOL','SHORT','LONG']) # => '########, ########, #.##....,########,########'
+solution(['INT','SHORT','FLOAT','INT','BOOL']) # => '########,##..####,########,#.......'
+solution(['FLOAT','SHORT','BOOL','BOOL','BOOL', 'INT']) # => '########,#.......,########'
+solution(['BOOL', 'LONG', 'SHORT', 'LONG', 'BOOL', 'LONG','BOOL','LONG','SHORT','LONG','LONG']) # => HALT
