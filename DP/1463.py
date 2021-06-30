c713b170ba7c4a84c5d81af6e1218023770fe393
minsgy <alstjrchl97@naver.com>
2021-06-30 19:12:45 +0900

    BOJ :: 1로만들기

---
+++ DP/1463.py
@@ -0,0 +1,18 @@
+def solution(x):
+
+  dp = [0] * (x+1)
+
+  for i in range(2, x+1):
+    dp[i] = dp[i - 1] + 1
+    if i % 2 == 0:
+      dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+      dp[i] = min(dp[i], dp[i // 3] + 1)
+    
+
+  print(dp[x])
+
+
+
+x = int(input())
+solution(x)
